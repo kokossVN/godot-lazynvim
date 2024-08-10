ea55336b504a6b635d8b3e5560403caf5b84f136
Kokoss <manh12345678951236789@gmail.com>
2024-08-10 14:17:23 +0700

    opts, tmux, more

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymaps = vim.keymap.set
+local opts = { silent = true, noremap = true }
+keymaps("i", "jk", "<ESC>")
+keymaps("n", "<TAB>", ":tabnext<CR>", opts)
+keymaps("n", "<S-TAB>", ":tabprev<CR>", opts)
+keymaps("n", "=", "<C-a>", opts)
+keymaps("n", "-", "<C-x>", opts)
+keymaps("i", "<A-h>", "<Left>")
+keymaps("i", "<A-j>", "<Down>")
+keymaps("i", "<A-k>", "<Up>")
+keymaps("i", "<A-l>", "<Right>")
+keymaps("i", "<ESC>", "`")
+keymaps("i", "`", "~")
--- lua/plugins/colorcheme.lua
+++ lua/plugins/colorcheme.lua
@@ -1,8 +1,16 @@
 return {
-  -- add gruvbox
-  { "catppuccin/nvim" },
+  -- add catppuccin
+  {
+    "catppuccin/nvim",
+    config = function()
+      local catppuccin = require("catppuccin")
+      catppuccin.setup({
+        transparent_background = true,
+      })
+    end,
+  },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
